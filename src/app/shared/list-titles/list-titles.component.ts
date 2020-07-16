f10130d883463093a01fbbb69335be2223cd24d8
Douglas <douglasnewmetal@yahoo.com.br>
2020-07-16 13:43:08 -0300

    sec

---
+++ src/app/shared/list-titles/list-titles.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-titles',
+  templateUrl: './list-titles.component.html',
+  styleUrls: ['./list-titles.component.scss']
+})
+export class ListTitlesComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
